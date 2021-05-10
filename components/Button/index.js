1ad51550565286cb13d3ee7bc8f1ed70108115d5
Arsy <pusaka.dk@gmail.com>
2021-05-11 02:54:19 +0700

    Fixed Responsive

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { FaArrowRight } from 'react-icons/fa';
 const Button = ({ text, ClickHandler, type, icon }) => {
     const onClickHandler = e => {
         ClickHandler();
@@ -8,19 +8,24 @@ const Button = ({ text, ClickHandler, type, icon }) => {
     return (
         <button
             onClick={onClickHandler}
-            className={`font-semibold px-4 py-3 rounded-lg transform transition-colors hover:bg-gray-200 flex items-center gap-2
+            className={`font-semibold group px-4 py-3 rounded-lg transform transition-colors hover:bg-gray-200 flex items-center gap-2
                 ${
                     type === 'primary' &&
-                    'bg-yellow-400 hover:bg-yellow-500 text-black'
+                    'bg-blue-400 hover:bg-blue-500 text-black'
                 }
                 ${
                     type === 'secondary' &&
                     'bg-none border-2 border-black hover:bg-gray-200 text-black'
                 }
+                ${type === 'more' && 'bg-none  hover:bg-gray-300 text-black'}
             `}>
             {icon && <div className='text-2xl'>{icon}</div>}
             {text}
-            {type === 'more' && <div className='text-2xl'>{icon}</div>}
+            {type === 'more' && (
+                <div className='text-xl text-blue-500 group-hover:'>
+                    <FaArrowRight />
+                </div>
+            )}
         </button>
     );
 };
